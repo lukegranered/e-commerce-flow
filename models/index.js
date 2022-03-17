d33078168a322908ac10e029595674aed4f7caad
Luke G <lukejgranered@gmail.com>
2022-03-17 16:04:37 -0500

    Models added

---
+++ models/index.js
@@ -0,0 +1,32 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'product_id'
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag_id'
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
