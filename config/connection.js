6d65993ea621938ce10b4a2279297964a1fe3b92
Luke G <lukejgranered@gmail.com>
2022-03-19 13:21:26 -0500

    server error fixed

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,9 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      username: 'root',
+      password: 'bootcamp',
+      database: 'ecommerce_db',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
